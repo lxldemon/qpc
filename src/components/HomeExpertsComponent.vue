e0e210d5d226d27578473c0f61c4125ce2554c68
cll123456 <2642137848@qq.com>
2021-07-12 16:38:03 +0800

    '完成轮播组件基本功能'

--- src/components/HomeExpertsComponent.vue
+++ src/components/HomeExpertsComponent.vue
@@ -1,88 +1,130 @@
 <template>
-    <div id="HomeExpertsComponent">
-        <div class="title">专家推荐</div>
-        <div class="content">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">Slide 1</div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                </div>
-                <!-- 如果需要分页器 -->
-                <div class="swiper-pagination"></div>
+  <div id="HomeExpertsComponent">
+    <div class="title">专家推荐</div>
+    <div
+      class="content"
+      @mouseenter="onMouseMoveEnter"
+      @mouseleave="onMouseMoveLeave"
+    >
+      <swiper class="swiper" :options="swiperOption" ref="mySwiper">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <swiper-slide>Slide 6</swiper-slide>
+        <swiper-slide>Slide 7</swiper-slide>
+        <swiper-slide>Slide 8</swiper-slide>
+        <swiper-slide>Slide 9</swiper-slide>
+        <swiper-slide>Slide 10</swiper-slide>
 
-                <!-- 如果需要导航按钮 -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-
-                <!-- 如果需要滚动条 -->
-                <div class="swiper-scrollbar"></div>
-            </div>
-        </div>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <!-- 如果需要导航按钮 -->
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
     </div>
+  </div>
 </template>
 
 <script>
-    import Swiper from 'swiper';   // Import Swiper styles
-    import 'swiper/dist/css/swiper.min.css';
-    export default {
-        components: {
-            Swiper,
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  name: "HomeExpertsComponent",
+  data() {
+    return {
+      swiperOption: {
+        // 自动播放
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
         },
-        name: "HomeExpertsComponent",
-        mounted(){
-            new Swiper ('.swiper-container', {
-                loop: true,
-                speed: 400,
-                spaceBetween: 100,
-                // 如果需要分页器
-                pagination: '.swiper-pagination',
-                // 如果需要前进后退按钮
-                nextButton: '.swiper-button-next',
-                prevButton: '.swiper-button-prev',
-                // 如果需要滚动条
-                scrollbar: '.swiper-scrollbar',
-            })
+        // 每一次的间隔
+        speed: 500,
+        // 循环
+        loop: true,
+        // 窗口显示几个
+        slidesPerView: 4,
+        // 每一个的边距
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
         },
-        methods: {
-            onSwiper(swiper) {
-                console.log(swiper);
-            },
-            onSlideChange() {
-                console.log('slide change');
-            },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
-
-    }
+      },
+    };
+  },
+  methods: {
+    //通过获得的swiper对象来暂停自动播放
+    /**
+     * 鼠标引入暂停播放
+     */
+    onMouseMoveEnter() {
+      this.$refs.mySwiper.swiperInstance.autoplay.stop();
+    },
+    /**
+     * 鼠标离开继续
+     */
+    onMouseMoveLeave() {
+      this.$refs.mySwiper.swiperInstance.autoplay.start();
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style scoped >
-    #HomeExpertsComponent{
-        margin: 0 auto;
-        margin-top: 31px;
-        width: 80%;
-        font-size: 18px;
-    }
-    #HomeExpertsComponent>.title{
-        float: left;
-    }
-    #HomeExpertsComponent>.content{
-        float: left;
-        clear: both;
-         width: 100%;
-         height: 300px;
-        margin: 20px auto;
-    }
-    .swiper-container{
-        height: 200px;
-        padding-left: 56px;
-        background-image: -webkit-radial-gradient(23% 100%, #2e79c3 0%, #297bc4 100%);
-    }
+#HomeExpertsComponent {
+  margin: 0 auto;
+  margin-top: 31px;
+  width: 80%;
+  font-size: 18px;
+}
+#HomeExpertsComponent > .title {
+  float: left;
+}
+#HomeExpertsComponent > .content {
+  float: left;
+  clear: both;
+  width: 100%;
+  height: 300px;
+  margin: 20px auto;
+}
+.swiper-container {
+  height: 200px;
+  /* padding-left: 56px;
+  padding-right: 56px; */
+  /* background-image: -webkit-radial-gradient(23% 100%, #2e79c3 0%, #297bc4 100%); */
+}
 
-    .swiper-container .swiper-slide .active {
-        color: #666;
-        background-color: #fff8eb;
-        box-shadow: 0 3px 13px 0 rgba(94, 94, 94, 0.50);
-    }
+/* .swiper-container .swiper-slide .active {
+  color: #666;
+  background-color: #fff8eb;
+  box-shadow: 0 3px 13px 0 rgba(94, 94, 94, 0.5);
+} */
 
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
