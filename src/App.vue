3b0a1e14a9f6684d6635efbc20884ecc8691f0ff
lxldemon <liuxiaox821@163.com>
2021-07-13 00:36:14 +0800

    tags 话题增加

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@
 body {
     margin: 0;
     padding: 0;
+    background: #f4f6f8;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -54,5 +55,24 @@ body {
     height: 50px;
     text-align: center;
     line-height: 50px;
+}
+::-webkit-scrollbar
+{
+    width: 5px;
+}
+
+/*定义滚动条轨道 内阴影+圆角*/
+::-webkit-scrollbar-track
+{
+    border-radius: 10px;
+    background-color: rgba(0,0,0,0.1);
+}
+
+/*定义滑块 内阴影+圆角*/
+::-webkit-scrollbar-thumb
+{
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    background-color: rgba(0,0,0,0.1);
 }
 </style>
